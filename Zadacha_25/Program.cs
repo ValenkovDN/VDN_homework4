87c6baf3ada77ca1aea59da9cb6544d95d214641
Дмитрий <rotweis87@gmail.com>
2022-09-11 13:52:34 +0300

    Добавил решение задачи №25

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.*/
+
+string NaturStep()
+{
+Console.Write("Ведите число A ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите число B ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a;
+      
+for(int i=0;i<b-1; i++)
+    {
+        c = c*a;
+    }
+
+return $"Число А в степени В равно {c}";
+}
+Console.WriteLine(NaturStep());
