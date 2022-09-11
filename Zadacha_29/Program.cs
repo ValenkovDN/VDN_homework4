8ebd1a0e7d67f1f1383576b069619958410bcc6c
Дмитрий <rotweis87@gmail.com>
2022-09-11 14:00:50 +0300

    Добавил решение задачи №29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+ и выводит их на экран.*/
+
+int[] array = new int[8];
+void FillArray(int[] massiv)
+{
+    Console.Write("Ведите восемь чисел для заполнения массива ");
+    int count = massiv.Length;
+    int index = 0;
+    while (index < count)
+    {
+       massiv [index] = Convert.ToInt32(Console.ReadLine());
+       index++;
+    }
+}
+
+void Vivod (int[] col)
+{
+   int caunt = col.Length;
+    for(int ind = 0;ind < caunt; ind++)
+    {
+       if(ind != col.Length - 1) Console.Write($"{col[ind]}, ");
+       else Console.WriteLine($"{col[ind]} ");
+    }
+}
+FillArray(array);
+Vivod (array);
